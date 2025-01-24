6d0d709f296405e5252fcdfd7be8a41f3119f450
AroonSachin <aroonfendi@gmail.com>
2025-01-24 05:52:51 -0500

    Made some minor changes for console output

--- src/http_interceptor.py
+++ src/http_interceptor.py
@@ -46,7 +46,7 @@ class InterceptEditProxy:
         """
         logger.info(f"Intercepted response: {flow.request.method} {flow.request.url}")
 
-        # Find the corresponding intercepted request to store the response
+        print("Finding the corresponding intercepted request to store the response")
         for call in reversed(intercepted_calls):
             if call["request"]["url"] == flow.request.url:
                 call["response"] = {
@@ -111,22 +111,22 @@ def generate_ca_certificate(cert_dir="mitmproxy-ca"):
     if not os.path.exists(cert_dir):
         os.makedirs(cert_dir)
 
-    # Generate the private key for the CA
+    print("Generating the private key for the CA certificate")
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
         backend=default_backend()
     )
 
-    # Save the private key
     with open(os.path.join(cert_dir, "ca_key.pem"), "wb") as key_file:
         key_file.write(private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
         ))
+    print("Private key saved")
 
-    # Generate the CA certificate
+    print("Generating the certificates")
     subject = issuer = x509.Name([
         x509.NameAttribute(x509.NameOID.COUNTRY_NAME, u"US"),
         x509.NameAttribute(x509.NameOID.STATE_OR_PROVINCE_NAME, u"California"),
@@ -138,11 +138,9 @@ def generate_ca_certificate(cert_dir="mitmproxy-ca"):
     cert = x509.CertificateBuilder().subject_name(subject).issuer_name(issuer).public_key(private_key.public_key()).serial_number(x509.random_serial_number()).not_valid_before(datetime.utcnow()).not_valid_after(datetime.utcnow() + timedelta(days=3650))  # Valid for 10 years
     cert = cert.add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True).sign(private_key, hashes.SHA256(), default_backend())
 
-    # Save the certificate
     cert_path = os.path.join(cert_dir, "ca_cert.pem")
     with open(cert_path, "wb") as cert_file:
         cert_file.write(cert.public_bytes(serialization.Encoding.PEM))
-
     print(f"CA private key and certificate have been generated in the '{cert_dir}' directory.")
     return cert_path
 
@@ -151,26 +149,25 @@ def transfer_ca_certificate(cert_path, remote_ip, remote_user, remote_password):
     """
     Transfer the CA certificate to a remote machine using SCP.
     """
-    # Using SCP to copy the certificate to the remote machine
+    print("Using SCP to copy the certificate to the remote machine")
     scp_command = f"scp {cert_path} {remote_user}@{remote_ip}:/tmp/mitmproxy-ca.crt"
     subprocess.run(scp_command, shell=True, check=True)
-
     print(f"Transferred CA certificate to {remote_ip}:/tmp/mitmproxy-ca.crt")
 
 def install_ca_certificate_remote(remote_ip, remote_user, remote_password):
     """
     Install the CA certificate on the remote machine using SSH.
     """
-    # Establish SSH connection to the remote machine
+    print("Establishing SSH connection to the remote machine")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(remote_ip, username=remote_user, password=remote_password)
 
-    # Install the CA certificate on the remote machine (Linux example)
+    print("Installing the CA certificate on the remote machine")
     install_command = "sudo cp /tmp/mitmproxy-ca.crt /usr/local/share/ca-certificates/mitmproxy-ca.crt && sudo update-ca-certificates"
     stdin, stdout, stderr = ssh.exec_command(install_command)
 
-    # Read the output and errors
+    print("Reading output and errors")
     output = stdout.read().decode()
     error = stderr.read().decode()
 
@@ -179,7 +176,7 @@ def install_ca_certificate_remote(remote_ip, remote_user, remote_password):
     else:
         print("CA certificate installed successfully.")
 
-    # Close the SSH connection
+    print("SSH connection closed")
     ssh.close()
 
 def parse_args():
